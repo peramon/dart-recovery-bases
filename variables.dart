d2c462682a5c0308e7141cfb2795018bed950421
Paul Ramon <peramon98.18@gmail.com>
2025-08-06 12:32:56 -0500

    review variables

---
+++ variables.dart
@@ -0,0 +1,16 @@
+void main() {
+  final String pokemon = 'Charizard';
+  final int hp = 100;
+  final bool? isAlive = true;
+  final List<String> skills = ['impostor'];
+  final sprites = <String>['ditto/front.png'];
+
+  //   print('Pokemon -> $pokemon');
+  print("""Pokemon Data
+  Pokemon -> $pokemon
+  Hp -> $hp
+  Is Alive -> $isAlive
+  Skills -> $skills
+  Sprite -> $sprites
+  """);
+}
