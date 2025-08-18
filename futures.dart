199b224656f18443252c8844aa9b7e478fd4b4f4
Paul Ramon <peramon98.18@gmail.com>
2025-08-18 14:54:32 -0500

    Async - await

--- futures.dart
+++ futures.dart
@@ -1,21 +1,35 @@
 // Futures -> Operacion Asincrono, es como una promesa
-void main() {
+void main() async {
   print('Inicion del programa');
 
-  httpGet('')
-      .then((value) {
-        print(value);
-      })
-      .catchError((err) {
-        print('Error: $err');
-      });
+  try {
+    final value = await httpGet('url');
+    print(value);
+  } catch (err) {
+    print('Tenemos un error: $err');
+  }
+  // httpGet('url')
+  //     .then((value) {
+  //       print(value);
+  //     })
+  //     .catchError((err) {
+  //       print('Error: $err');
+  //     });
 
   print('Fin del programa');
 }
 
-Future<String> httpGet(String url) {
-  return Future.delayed(const Duration(seconds: 2), () {
-    throw 'Error en la peticion http';
-    // return 'Respuesta de la peticion http';
-  });
+Future<String> httpGet(String url) async {
+  await Future.delayed(const Duration(seconds: 2));
+
+  throw 'Error en la peticion';
+
+  // return 'Tenemos un valor de la peticion';
 }
+
+// Future<String> httpGet(String url) async {
+//   return Future.delayed(const Duration(seconds: 2), () {
+//     throw 'Error en la peticion http';
+//     // return 'Respuesta de la peticion http';
+//   });
+// }
