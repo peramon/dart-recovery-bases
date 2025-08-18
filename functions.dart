d9b30db5c427fe185d7a8a0726760dc74358bdca
Paul Ramon <peramon98.18@gmail.com>
2025-08-18 08:34:36 -0500

    Parametros con nombres

--- functions.dart
+++ functions.dart
@@ -1,6 +1,20 @@
 // functions
-void main() {}
+void main() {
+  print(greetEveryone());
+  print('Plus -> ${addTwoNumbers(5, 9)}');
+  print(greetPerson(message: 'How are you'));
+}
 
-greetEveryone() {
-  return 'Hello everyone';
+String greetEveryone() => 'Hello everyone';
+
+int addTwoNumbers(int a, int b) => a + b;
+
+int addTwoNumbersOptional(int a, [int b = 0]) {
+  // b = b ?? 0;
+  // b ??= 0;
+  return a + b;
+}
+
+String greetPerson({String? message, String? name = 'Paul'}) {
+  return '${message}, ${name}';
 }
